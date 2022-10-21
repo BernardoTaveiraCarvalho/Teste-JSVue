e2d9e55986cd09fe5aff7f2c0e76ae5ac61f6d17
BernardoTaveiraCarvalho <bernardo.taveira.carvalho@gmail.com>
2022-10-21 19:20:47 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script>
+
+import Main from './components/Main.vue';
+ 
+
+
+  export default {
+    name:"App",
+    components: {
+   
+    Main
+},
+    data(){
+      return{
+      
+      }
+    },
+   
+  }
+  </script>
+  
+  <template>
+    <h1>Teste Vue</h1>
+  <Main></Main>
+   </template>
---
+++ src/components/Main.vue
@@ -0,0 +1,37 @@
+<script>
+ 
+ 
+ import {RouterView,RouterLink} from 'vue-router';
+ 
+  export default {
+    name:"App",
+    components: {
+    
+    },
+    created() {
+       
+        },
+    data(){
+      return{
+      
+      }
+    },
+    methods: {
+  
+    },
+    computed: {
+ 
+}, 
+  }
+  </script>
+  
+  <template>
+   
+    
+
+   
+
+   </template>
+<style>
+
+</style>
